e9f7e30f8ab97de2b22756cdcc7c8e8ee5278d82
ElitLien <supervlad586@gmail.com>
2021-05-16 21:49:03 +0300

    first master commit

---
+++ Lab_5.1(B)/Error.h
@@ -0,0 +1,19 @@
+///////////
+// Error.h
+///////////
+
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Error
+{
+	string name;
+public:
+	Error(string name)
+		:name(name) {}
+	string what() const { return name; }
+};
+
---
+++ UnitTest5.1(B)/UnitTest5.1(B).cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.1(B)/Vector2D.h"
+#include "../Lab_5.1(B)/Vector2D.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51B
+{
+	TEST_CLASS(UnitTest51B)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Vector2D a(1, 0);
+			++a;
+			Assert::AreEqual(a.getX(), 2.0);
+		}
+	};
+}
